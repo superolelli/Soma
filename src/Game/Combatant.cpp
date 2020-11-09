f6e8bda9eb2b33bd7936471dfe3f04465670f575
coledea <olewegen@gmx.de>
2020-11-09 18:21:14 +0100

    Critical Hit Information

--- src/Game/Combatant.cpp
+++ src/Game/Combatant.cpp
@@ -291,6 +291,7 @@ void Combatant::ApplyAbilityEffect(Combatant * _attacker, AbilityEffect & _effec
 	bool isCriticalHit = rand() % 100 < _attacker->status.GetCriticalHit() + _effect.criticalHitModificator;
 	if (isCriticalHit)
 	{
+		notificationRenderer->AddNotification("Kritisch!", g_pFonts->f_kingArthur, sf::Vector2f(GetRect().left + GetRect().width / 2.0f, GetRect().top), 1.0f);
 		g_pSounds->PlaySound(CRITICAL_HIT);
 		criticalEffectFactor = 2;
 		criticalDurationFactor = 1.5;
